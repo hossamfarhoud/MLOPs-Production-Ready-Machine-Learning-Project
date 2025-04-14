3f37e25ba649e1307df89a766cc6d237f5a3b63f
Hossam <alshshtawyfrhwd12@gmail.com>
2025-04-14 13:00:11 -0700

    last_update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="us_visa",
     version="0.0.0",
-    author="Bappy",
-    author_email="entbappy73@gmail.com",
+    author="Hossam_Farhoud",
+    author_email="hossam.alsheshtawy2000@gmail.com",
     packages=find_packages()
 )
